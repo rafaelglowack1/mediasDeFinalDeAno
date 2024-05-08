ba774e708f86bc7b2d2d0a88baf7081fb951e22c
rafaelglowack1 <rafaelglowacki555@gmail.com>
2024-05-07 21:22:23 -0300

    1.4

--- script.js
+++ script.js
@@ -1,24 +1,57 @@
 let formulario = document.querySelector('#formulario')
 var linhas = ''
-  
+const valorNota = []
+const valorNome = []
+
+const spanaprovado = '<span class="aprovado">aprovado</span>'
+const spanreprovado = '<span class="reprovado">reprovado</span>' 
 formulario.addEventListener("submit" , function(e){
     e.preventDefault()
+    addLinha()
+    addPagina()
+    atualisaMedia()
     
-    const nome = document.querySelector("#nome")
-    const nota = document.querySelector('#nota')
+})
 
-  
-
-    var linha = '<tr>'
-    linha+= `<td> ${ nome.value} </td>`
-    linha+= `<td> ${nota.value}</td>`
-    linha+= `<td> ${nota.value >= 7 ? '😁' : '🥶'} </td>`
-    linha+='</tr>'
-    linhas+=linha
-
-    let tabela = document.querySelector('#tbody')
+function addPagina(){
+      let tabela = document.querySelector('#tbody')
     tabela.innerHTML=linhas
 
     nota.value=''
     nome.value=''
-})
+}
+ 
+function addLinha(){
+    const nome = document.querySelector("#nome")
+    const nota = document.querySelector('#nota')
+
+    if (valorNome.includes(nome.value)){
+        alert('já tem')
+    }else{
+        valorNome.push(nome.value)
+        valorNota.push(Number(nota.value))
+
+        var linha = '<tr>'
+        linha+= `<td> ${ nome.value} </td>`
+        linha+= `<td> ${nota.value}</td>`
+        linha+= `<td> ${nota.value >= 7 ? '😁' : '🥶'} </td>`
+        linha+='</tr>'
+        linhas+=linha
+    }
+  
+}
+function atualisaMedia(){
+    let mediafinal = calculoMedia()
+
+    document.querySelector('#media').innerHTML = mediafinal
+    document.querySelector('#resultado').innerHTML= mediafinal >=7 ? spanaprovado : spanreprovado ;
+}
+function calculoMedia(){
+    let somaNotas = 0
+
+    for (let i = 0 ; i < valorNota.length ; i++){
+        somaNotas += valorNota[i]
+        
+    }
+    return somaNotas / valorNota.length
+}
